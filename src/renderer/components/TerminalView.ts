4b61a9d112bfd21dbef85cb09cab3219a3dfc941
raddle60 <raddle60@users.noreply.github.com>
2026-06-03 17:47:28 +0800

    refactor: use VSCode xterm.css for proper terminal styling

--- src/renderer/components/TerminalView.ts
+++ src/renderer/components/TerminalView.ts
@@ -1,6 +1,5 @@
 import { Terminal } from '@xterm/xterm';
 import { FitAddon } from '@xterm/addon-fit';
-import '@xterm/xterm/css/xterm.css';
 
 export class TerminalView {
   private terminal: Terminal;
