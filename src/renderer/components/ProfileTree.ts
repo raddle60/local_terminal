949ffe83b457958f1e55898cd3f594f27175dbd5
raddle60 <raddle60@users.noreply.github.com>
2026-06-16 14:56:36 +0800

    修改文件夹样式

--- src/renderer/components/ProfileTree.ts
+++ src/renderer/components/ProfileTree.ts
@@ -112,7 +112,9 @@ export class ProfileTree {
         folderHeader.className = 'folder-header';
         const icon = document.createElement('span');
         icon.className = 'folder-icon';
-        icon.textContent = this.expandedFolders.has(node.id) ? '📂' : '📁';
+        icon.innerHTML = this.expandedFolders.has(node.id)
+          ? '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/><path d="M2 10h20"/></svg>'
+          : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/></svg>';
         const name = document.createElement('span');
         name.className = 'folder-name';
         name.textContent = node.name;
@@ -185,10 +187,10 @@ export class ProfileTree {
     const isExpanding = !this.expandedFolders.has(id);
     if (this.expandedFolders.has(id)) {
       this.expandedFolders.delete(id);
-      icon.textContent = '📁';
+      icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/></svg>';
     } else {
       this.expandedFolders.add(id);
-      icon.textContent = '📂';
+      icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/><path d="M2 10h20"/></svg>';
     }
     this.folderToggleListeners.forEach(l => l(id, isExpanding));
     this.render();
